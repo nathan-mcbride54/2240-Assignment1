339a29a6c329b19fe6c558869f148ce51d9fbee6
Nathan McBride <nmcbride@protonmail.com>
2019-02-02 12:04:48 -0500

    Added iterative method

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -1,3 +1,17 @@
 public class fibonacci {
     
+    public static long fiboSeriesIte(long iterations){
+        long startTime = System.nanoTime();
+        int n1=0, n2=1, n3, i ;
+        System.out.print(n1+" "+n2);
+        for(i=2;i<iterations;++i)
+        {
+            n3=n1+n2;
+            System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
+        }
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
 }
