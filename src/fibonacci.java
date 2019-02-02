9f36f0e571f45ab1f088fe46c667ef631ff9bdef
Nathan McBride <nmcbride@protonmail.com>
2019-02-02 12:05:45 -0500

    Added recursive method

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -1,5 +1,5 @@
 public class fibonacci {
-    
+
     public static long fiboSeriesIte(long iterations){
         long startTime = System.nanoTime();
         int n1=0, n2=1, n3, i ;
@@ -13,5 +13,24 @@ public class fibonacci {
         }
         long endTime = System.nanoTime();
         return endTime - startTime;
+    }
+
+    public static long fiboSeriesRec(long x, boolean print){
+
+        if (x==0||x==1) {
+            if (print){
+                System.out.print(x +" ");
+            }
+        }
+        else {
+            if (print) {
+                x = fiboSeriesRec(  x -1 , true ) + fiboSeriesRec(x -2 , false);
+                System.out.print(x + " ");
+            }
+            else {
+                x = fiboSeriesRec(  x -1 , false ) + fiboSeriesRec(x -2 , false);
+            }
+        }
+        return x;
     }
 }
